0e3d312ace99a92e3374978b817d55410dabe42b
Vishnu-C-Prasad <mr.vishnucp001@outlook.com>
2020-09-30 13:17:12 +0530

    Added: Sections for manage product like update product, delete product, add  product

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,16 +1,40 @@
 var db = require('../config/connection');
-var colleciton = require('../config/collection');
+var collection = require('../config/collection');
+var ObjectID =require('mongodb').ObjectID;
 
 module.exports = {
     addProduct: (product, callback) => {
-        db.get().collection(colleciton.PRODUCT_COLLECTION).insertOne(product).then((data) => {
+        db.get().collection(collection.PRODUCT_COLLECTION).insertOne(product).then((data) => {
             callback(data.ops[0]._id);
         });
     },
     getProduct: () => {
         return new Promise( async (resolve, reject) => {
-            const products = await db.get().collection(colleciton.PRODUCT_COLLECTION).find().toArray();
+            const products = await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray();
             resolve(products);
+        });
+    },
+    updateProduct: (data) => {
+        return new Promise(async (resolve, reject) => {
+            db.get().collection(collection.PRODUCT_COLLECTION).updateOne({
+                _id: new ObjectID(data._id)
+            }, {
+                $set: {
+                    name: data.name,
+                    category: data.category,
+                    description: data.description,
+                    price: data.price
+                }
+            }).then((data) => {
+                resolve(data);
+            });
+        });
+    },
+    deleteProduct: (id) => {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collection.PRODUCT_COLLECTION).deleteOne({ _id: new ObjectID(id) }).then(async (data) => {
+                    resolve(data);
+            });
         });
     }
 }
--- routes/admin/admin-products.js
+++ routes/admin/admin-products.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 var productHelpers = require('../../helpers/product-helpers');
 
 router.get('/all-products', function (req, res, next) {
@@ -26,6 +27,49 @@ router.post('/add-product', function (req, res) {
       }
     });
 
+  });
+});
+
+router.get('/update-product', function (req, res) {
+  productHelpers.getProduct().then((products) => {
+    res.render('admin/update-product', { title: 'AdminPanel | Update Product', admin: true, products });
+  });
+});
+
+router.post('/update-product', function (req, res) {
+  if (req.files) {
+    fs.unlinkSync(`./public/images/product-images/${req.body._id}.jpg`);
+
+    let image = req.files.image;
+
+    image.mv(`./public/images/product-images/${req.body._id}.jpg`, (err, done) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(done);
+      }
+    });
+  }
+
+  productHelpers.updateProduct(req.body).then(() => {
+    productHelpers.getProduct().then((products) => {
+      res.render('admin/update-product', { title: 'AdminPanel | Update Product', admin: true, products });
+    });
+  });
+});
+
+router.get('/delete-product', function (req, res) {
+  productHelpers.getProduct().then((products) => {
+    res.render('admin/delete-product', { title: 'Adminpanel | Delete Product', admin: true, products });
+  });
+});
+
+router.post('/delete-product', function (req, res) {
+  fs.unlinkSync(`./public/images/product-images/${req.body._id}.jpg`);
+  productHelpers.deleteProduct(req.body._id).then(() => {
+    productHelpers.getProduct().then((products) => {
+      res.render('admin/delete-product', { title: 'AdminPanel | Delete Product', admin: true, products });
+    });
   });
 });
 
